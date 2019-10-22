b2f69b49589d3fe5834e023ab520d5aaaeaf41f3
Giovani Del Nero Diniz <gdiniz@bbn.com>
2019-10-22 14:09:19 -0400

    [FIX] heading

--- src/position_controller.py
+++ src/position_controller.py
@@ -56,18 +56,18 @@ class PositionController:
 
                 theta = (2.0 * math.pi - math.atan2(y, x))
 
-                # here we make sure we align the rover with the 
-                if (theta >= math.pi / 2.0) and (theta <= 3.0 * math.pi / 2.0):
-
-                    theta = theta + math.pi
-
-                    x = 0.0
-
-                    y = 0.0
-
             else:
 
                 theta = (2.0 * math.pi - target.heading)
+
+            # here we make sure we align the rover with the 
+            if (theta >= math.pi / 2.0) and (theta <= 3.0 * math.pi / 2.0):
+
+                theta = theta + math.pi
+
+                x = 0.0
+
+                y = 0.0
 
         quaternion = tf.transformations.quaternion_from_euler(0.0, 0.0, theta)
 
