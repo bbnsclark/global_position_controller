b9f2162fd83ce689d0b98c98c60ed83aa43ce9a7
Giovani Del Nero Diniz <gdiniz@bbn.com>
2019-11-22 10:12:55 -0500

    [TEST]

--- src/node.py
+++ src/node.py
@@ -129,7 +129,7 @@ class Node:
                 self.pub_goal.publish(self.new_goal)
 
             # wait 5 seconds
-            time.sleep(1.0)
+            time.sleep(10.0)
 
         # once the platform has reached its goal we cancel all move_base goals
         self.client_goal.cancel_goal()
